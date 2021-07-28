d565a584c0def28c17bc162424c281b7342eb343
Marc Totaro <marctotaro94@gmail.com>
2021-07-27 21:46:32 -0700

    changed to namespace

--- main.py
+++ main.py
@@ -11,31 +11,39 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
+# webdriver setup
+
 WEBDRIVER_PATH = "C:\Testing\chromedriver.exe"
 driver = webdriver.Chrome(WEBDRIVER_PATH)
 
-driver.get("https://techwithtim.net")
-print(driver.title)
 
-search = driver.find_element_by_name("s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
+if __name__ == "__main__":
 
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
-    )
+    driver.get("https://techwithtim.net")
+    print(driver.title)
+
+    # --- Search Function ---
+
+    search = driver.find_element_by_name("s")
+    search.send_keys("test")
+    search.send_keys(Keys.RETURN)
     
-    articles = main.find_elements_by_tag_name("article")
-    for article in articles:
-        header = article.find_element_by_class_name("entry-summary")
-        print(header.text)
-    
-except:
+    try:
+        main = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "main"))
+        )
+        
+        articles = main.find_elements_by_tag_name("article")
+        for article in articles:
+            header = article.find_element_by_class_name("entry-summary")
+            print(header.text)
+        
+    except:
+        driver.quit()
+
+
+    # --- wait and teardown ---
+    time.sleep(5)
+
     driver.quit()
-
-
-
-time.sleep(5)
-
-driver.quit()
